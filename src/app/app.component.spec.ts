eebdd5871c29cbe95deb82ecd840b350e310361e
pmislinfreefr <pmislin@free.fr>
2018-10-06 18:56:38 +0200

    test operational

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { WeatherService } from './weather.service';
+import { WeatherServiceFake } from './weather.service.fake';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        CurrentWeatherComponent
       ],
+      providers: [{ provide: WeatherService, useClass: WeatherServiceFake}]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
